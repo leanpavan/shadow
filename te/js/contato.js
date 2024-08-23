a60f81687530b7480592e317a06a57b662886e51
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2024-08-23 10:13:33 -0300

    upd forms required system

--- te/js/contato.js
+++ te/js/contato.js
@@ -1,3 +1,16 @@
+const txtName = document.getElementById("nome");
+const txtSobrenome = document.getElementById("sobrenome");
+const txtEmail = document.getElementById("email");
+const number = document.getElementById("telefone");
+const mensagem = document.getElementById("mensagem");
+const arquivo = document.getElementById("arquivo");
+
+function SubmitForms()
+{
+
+}
+
+
 function formatarTelefone(telefone) {
     telefone = telefone.replace(/\D/g, '');
     telefone = telefone.replace(/^(\d{2})(\d)/g, '($1) $2');
@@ -9,3 +22,4 @@ function aplicarMascara(event) {
     const campo = event.target;
     campo.value = formatarTelefone(campo.value);
 }
+
